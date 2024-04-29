6f426128fd8f7da3eb91ba5bf85415a584f72665
S A U R A V <dwarmachine24@gmail.com>
2024-04-29 17:21:45 +0530

    Update husky-vendor.mk

--- husky-vendor.mk
+++ husky-vendor.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2023 \
     DiagMon \
     EuiccGoogle \
@@ -106,7 +105,6 @@ PRODUCT_PACKAGES += \
     LLKAgent \
     MyVerizonServices \
     OemRilService \
-    PixelDisplayService \
     PixelQualifiedNetworksService \
     RilConfigService \
     ShannonIms \
